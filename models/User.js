785ca95dbbb30eef850a41304cc17ea956bee2be
GeekCentric <ts22082@gmail.com>
2019-04-25 08:00:41 -0700

    create profile schema

--- models/User.js
+++ models/User.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const UserSchema = {
+const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-  todos: [
-    {
-      subject: { type: String, required: true },
-      text: { type: String, required: true },
-      date: { type: Date, default: Date.now }
-    }
-  ]
-}
+  date: { type: Date, default: Date.now }
+})
 
 module.exports = User = mongoose.model('users', UserSchema)
